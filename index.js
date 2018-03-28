c4c552d05b49787e55242f6cbbc5c71111311053
Michael B. Young <mikebyoung80@comcast.net>
2018-03-28 16:28:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 add()1 + 80 // 81
 subtract()60 - 40 // 20
-multiply(2 * 3.4) // 6.8 (there's that floating-point arithmetic again...)
+multiply()2 * 3.4 // 6.8 (there's that floating-point arithmetic again...)
 divide(5.0 / 2.5) // 2
 
 
