91e96a56532fccf8725620888ad4abaf663ebef5
Michael B. Young <mikebyoung80@comcast.net>
2018-03-28 16:26:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-1 + 80 // 81
-60 - 40 // 20
-2 * 3.4 // 6.8 (there's that floating-point arithmetic again...)
-5.0 / 2.5 // 2
+add(1 + 80) // 81
+subtract(60 - 40) // 20
+multiply(2 * 3.4) // 6.8 (there's that floating-point arithmetic again...)
+divide(5.0 / 2.5) // 2
 
 
 var number = 5
