875b88079ae4dbc3ff64013eee030d446057814c
Michael B. Young <mikebyoung80@comcast.net>
2018-03-28 16:42:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,44 +12,5 @@ function divide(a,b){
 return a / b;
 }
 
-var number = 5
- 
-number++ // 5... hmmmm
- 
-number // 6 -- the number was incremented after it was evaluated
- 
-number-- // 6
- 
-number // 5
-
-
-
-
-var nuimber = 10
-
-function add5() {
-  number += 5
-}
-function divideBy3() {
-  number /= 3
-}
-divideBy3()
-
-console.log(number) //3.333333335
-
-add5()
-
-console.log(number) //8.33333333335
-
-// reset number
-number = 10
-
-add5()
-
-console.log(number) // 15
-
-divideBy3()
-
-console.log(number) // 5
 
 
